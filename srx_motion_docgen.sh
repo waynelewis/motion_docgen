acb0fbb1e165bed111ee89c3d316dca44e0e7eab
Wayne Lewis <wlewis@bnl.gov>
2015-01-27 08:43:02 -0500

    Add argument for output file.

--- srx_motion_docgen.sh
+++ srx_motion_docgen.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-python motion_docgen.py '/Users/wlewis/data/work/beamlines/srx/commissioning/PS-R-XFD-SRX-RPT-002.xlsx'
+python motion_docgen.py --file-path '/Users/wlewis/data/work/beamlines/srx/commissioning/PS-R-XFD-SRX-RPT-002.xlsx' --output-file = 'srx_motion_doc'
